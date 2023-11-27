ff544f8f79ed461f0d484a8ed8e485c09bcd69b4
Mandala <gouthami.mandala@capgemini.com>
2023-11-27 13:44:04 +0530

    second commit

---
+++ EmployeeManagementApp-main/src/main/java/com/employeemanagement/pack/EmployeeManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementAppBackendApplication.class, args);
+	}
+
+}
---
+++ EmployeeManagementApp-main/src/test/java/com/employeemanagement/pack/EmployeeManagementAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
